61e41b8621669811fe596e7ba7ee0c56d3b416ed
Gustavo Gonçalves <nascimentoguga64@gmail.com>
2025-12-03 21:42:02 -0300

    ajustando o os number do js

--- js/04-atividade.js
+++ js/04-atividade.js
@@ -1,13 +1,11 @@
-let n1 = Number(prompt("Primeiro número:"));
-let n2 = Number(prompt("Segundo número:"));
+let n1 = Number(prompt("Digite o Primeiro número:"));
+let n2 = Number(prompt("Digite o Segundo número:"));
 
 if (n1 > n2) {
     console.log("O número maior é: " + n1)
-  alert ("O número maior é: " + n1)
 } else if (n2 > n1){
     console.log("O número maior é: " + n2)
-    alert ("O número maior é: " + n2)
 } else {
     console.log("São igauis")
-    alert (`São iguais!`)
 }
+
--- js/07-atividade.js
+++ js/07-atividade.js
@@ -1,34 +1,17 @@
-let n1 = Number(prompt("Primeiro número:"))
-let n2 = Number(prompt("Segundo número:"))
-let op = prompt("Operação(+, -, *, /)")
+let n3 = Number(prompt("Digite o Primeiro número:"));
+let n4 = Number(prompt(" Digite o Segundo número:"));
+let operacao = prompt("Digite a operação desejada: (+, -, *, /)");
 
-switch (op){
-case "+":
-    let soma = n1 + n2;
-alert(`O resultado da soma é: ${soma}`)
-    console.log("O resultado da soma é: " + soma)
-   
-    break
-    
-case "-":
-    let subtracao = n1 - n2;
-alert(`O resultado da subtração é: ${subtracao}`)
-    console.log("O resultado da subtração é: " + subtracao)
-    break
-
-case "*":
-    let multiplicacao = n1 * n2;
-alert(`O resultado da multiplicação é: ${multiplicacao}`)
-    console.log("O resultado da multiplicação é: " + multiplicacao)
-    break
-
-case "/":
-      let divisao = n1 / n2;
-alert(`O resultado da divisão é: ${divisao}`)
-    console.log("O resultado da divisão é: " + divisao)
-    break
-
-default:
+let resultado;
+if (operacao === "+") {
+    console.log(n3 + n4);
+} else if (operacao === "-") {
+    console.log(n3 - n4);
+} else if (operacao === "*") {
+    console.log(n3 * n4); 
+} else if (operacao === "/") {
+    console.log(n3 / n4);
+} else {
     console.log("Operação inválida!")
-    alert(`operação inválida!`)
 }
+
